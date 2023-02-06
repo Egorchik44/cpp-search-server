8ce756da758c0d597ebb1b27e569bb61df94ca53
Egorchik44 <111063360+Egorchik44@users.noreply.github.com>
2023-02-06 21:21:14 +0300

    Add files via upload

--- search-server/remove_duplicates.cpp
+++ search-server/remove_duplicates.cpp
@@ -1,18 +1,30 @@
 #include "remove_duplicates.h"
+#include "document.h"
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
-
-void RemoveDuplicates(SearchServer& search_server){
-    std::set<int> remove_;
-    auto words_with_ids_ = search_server.GetWordsWithIds();
-    int count = (search_server.end() - search_server.begin());  
-    for(int i = 0; i < count; i++){
-            if(words_with_ids_[i] == search_server.GetWordsWithIds()[i+1]){
-                remove_.insert(i+1);
-            }
+void RemoveDuplicates(SearchServer& search_server) {
+  vector <pair<int, vector<string>>>  remove_;
+  for (const int document_id : search_server.GetDocument_ids()){
+    vector<string> words_;
+    auto lots_of_words_ = search_server.GetWordFrequencies(document_id);
+    for (auto iterator = lots_of_words_.begin(); iterator != lots_of_words_.end(); iterator++){
+    	words_.push_back(iterator->first);
     }
-    for(auto i : remove_){
-        std::cout << "Found duplicate document id " << i << std::endl;
-        search_server.RemoveDocument(i);
+    remove_.push_back(pair(document_id, words_));
+  }
+   for (int i = 0; i < static_cast<int>(remove_.size()); i++){
+    for (int j = i + 1; j < static_cast<int>(remove_.size()); j++){
+      if (remove_[i].second == remove_[j].second) {
+        cout << "Found duplicate document id " << remove_[j].first << endl;
+        search_server.RemoveDocument(remove_[j].first);
+        remove_.erase(remove_.begin() + j);
+        j--;
+      }
     }
+  }
 }
-
